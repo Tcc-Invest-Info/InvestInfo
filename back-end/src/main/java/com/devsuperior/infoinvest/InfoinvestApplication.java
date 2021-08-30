3b3f4bfbf97922ebee3536554fcfe727811d2e0b
MVBPinho <marcos.vb.pinho@live.com>
2021-08-30 20:44:25 -0300

    commit inicial do back-end. Criado o método de segurança para liberar os cors e os arquivos properties para comunicação com o banco de dados

---
+++ back-end/src/main/java/com/devsuperior/infoinvest/InfoinvestApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.infoinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoinvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoinvestApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/devsuperior/infoinvest/InfoinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.infoinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoinvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
