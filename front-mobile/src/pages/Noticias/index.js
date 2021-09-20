8d1fd38f0a238cd53d839c75a802b66936718f0a
borgesddr <lucasborgesantonioo@gmail.com>
2021-09-20 20:43:46 -0300

    Tela de noticias pronta

--- front-mobile/src/pages/Noticias/index.js
+++ front-mobile/src/pages/Noticias/index.js
@@ -3,12 +3,11 @@ import {
         ImageBackground,
         View, 
         Text, 
-        TouchableOpacity,
+        Linking,
         ScrollView
 } from 'react-native'
 import backgroundImage from '../../../assets/noticias.png'
 import styles from './styles'
-import { Entypo } from '@expo/vector-icons';
 
 export default function ChatBoot ({navigation}){
 
@@ -21,65 +20,87 @@ export default function ChatBoot ({navigation}){
             </View>
             <ScrollView style={styles.scrollView}>
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://economia.uol.com.br/cotacoes/cambio/')
+                        }}>
+                        Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
+                        tabelas e histórico de cotações. Conheça também o conversor de moedas.
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://www.infomoney.com.br/mercados/ibovespa-cai-forte-em-meio-a-preocupacoes-com-divida-da-evergrande-e-nova-queda-do-minerio-dolar-sobe-a-r-532/')
+                        }}>
+                       Ibovespa cai mais de 3% seguindo exterior em meio a caso Evergrande; dólar sobe a R$ 5,32
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://g1.globo.com/economia/noticia/2021/09/20/bovespa.ghtml')
+                        }}>
+                       Bovespa cai mais de 2% e fecha na pior pontuação do ano
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://economia.uol.com.br/cotacoes/noticias/redacao/2021/09/20/fechamento-dolar-ibovespa-20-setembro.htm')
+                        }}>
+                       Dólar sobe 0,93%, a R$ 5,331, com temor sobre Evergrande; Bolsa tomba 2,33
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://www.infomoney.com.br/guias/mercado-de-acoes/')
+                        }}>
+                      Entenda como funciona o mercado de ações e a bolsa de valores
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://economia.uol.com.br/cotacoes/cambio/')
+                        }}>
+                        Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
+                        tabelas e histórico de cotações. Conheça também o conversor de moedas.
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://www.infomoney.com.br/mercados/ibovespa-cai-forte-em-meio-a-preocupacoes-com-divida-da-evergrande-e-nova-queda-do-minerio-dolar-sobe-a-r-532/')
+                        }}>
+                       Ibovespa cai mais de 3% seguindo exterior em meio a caso Evergrande; dólar sobe a R$ 5,32
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://g1.globo.com/economia/noticia/2021/09/20/bovespa.ghtml')
+                        }}>
+                       Bovespa cai mais de 2% e fecha na pior pontuação do ano
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://economia.uol.com.br/cotacoes/noticias/redacao/2021/09/20/fechamento-dolar-ibovespa-20-setembro.htm')
+                        }}>
+                       Dólar sobe 0,93%, a R$ 5,331, com temor sobre Evergrande; Bolsa tomba 2,33
                     </Text>
-                </View>                
+                </View>                                                                                 
                 <View style={styles.container_Noticias}>
-                    <Text style={styles.noticias}>
-                     Dólar Comercial: Acompanhe no UOL Economia a cotação de hoje, veja gráficos,
-                     tabelas e histórico de cotações. Conheça também o conversor de moedas.
+                    <Text style={styles.noticias}
+                     onPress={()=>{
+                          Linking.openURL('https://www.infomoney.com.br/guias/mercado-de-acoes/')
+                        }}>
+                      Entenda como funciona o mercado de ações e a bolsa de valores
                     </Text>
-                </View>                
+                </View>                                                                                 
             </ScrollView>
         </View>
     )
